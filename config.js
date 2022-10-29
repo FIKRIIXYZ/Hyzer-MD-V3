66b3212ab36a02913029217436eb1a8ee75e2df9
FIKRIIXYZ <116017493+FIKRIIXYZ@users.noreply.github.com>
2022-10-29 15:30:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -67,7 +67,7 @@ global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
   'https://melcanz.com': 'elaina',
   'https://server-api-rey.herokuapp.com': 'apirey',
-  'https://api.xteam.xyz': 'd37372311698ed1d',
+  'https://api.xteam.xyz': '3379f34db8a03321',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://bsbt-api-rest.herokuapp.com': 'benniismael',
   'https://api.zeks.xyz': 'apivinz',

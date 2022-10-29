dc903eb40bb0efbd7b09b1ba601eb8f834f2ba35
FIKRIIXYZ <116017493+FIKRIIXYZ@users.noreply.github.com>
2022-10-29 14:44:30 +0700

    Update main-donasi.js

--- plugins/main-donasi.js
+++ plugins/main-donasi.js
@@ -1,9 +1,7 @@
 let fs = require('fs')
 let handler = async (m, { conn }) => {
 let teks = 'donasi'
-let dana = global.dana
 let pulsa = global.pulsa
-let gopay = global.gopay
 let numberowner = global.numberowner
 let anu = `Owner nya sultan gausah donasi
 

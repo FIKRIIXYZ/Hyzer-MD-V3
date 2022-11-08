b5e1b45eb2493596dbef592b469a3edf6205b348
FIKRIIXYZ <116017493+FIKRIIXYZ@users.noreply.github.com>
2022-11-08 17:14:37 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,57 +1,25 @@
-let moment = require('moment-timezone')
-const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
-let fs = require('fs')
 let fetch = require('node-fetch')
-let handler = async (m, {text}) => {
-if (!text) return conn.reply(m.chat, 'Harap masukkan link\n\nContoh: .tiktok https://tiktok.com/xxxxxx', m)
-
-    let who
-    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
-    else who = m.sender
-    let user = global.db.data.users[who]
-    let more = String.fromCharCode(8206)
-    let readMore = more.repeat(4001)
-let anu = `*── 「 TIKTOK 」 ──*
-
-SILAHKAN PILIH`
-     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
-     templateMessage: {
-         hydratedTemplate: {
-           hydratedContentText: anu,
-           locationMessage: { 
-           jpegThumbnail: fs.readFileSync('./src/welcome.jpg')}, 
-           hydratedFooterText: wm,
-           hydratedButtons: [{
-             urlButton: {
-               displayText: '📍instagram',
-               url: instagram
-               }
-               
-             },
-             {
-             quickReplyButton: {
-               displayText: 'Tanpa Watermark',
-               id: `.tiktoknowm ${text}`,
-             }
-           },
-           {
-             quickReplyButton: {
-               displayText: 'dengan watermark',
-               id: `.tiktokwm ${text}`,
-             }
-           }]
-         }
-       }
-     }), { userJid: m.sender, quoted: m });
-    //conn.reply(m.chat, text.trim(), m)
-    return await conn.relayMessage(
-         m.chat,
-         template.message,
-         { messageId: template.key.id }
-     )
+let handler = async (m, { itsu, args }) => {
+if (!args[0]) throw 'Uhm..url nya mana?'
+m.reply(wait)
+let res = await fetch(`https://api.lolhuman.xyz/api/tiktok?apikey=KingOfBear&url=${args[0]}`)
+if (!res.ok) throw await res.text()
+let json = await res.json()
+if (!json.status) throw json
+let { description, author, statistic, link } = json.result
+await itsu.sendFile(m.chat, link, 'tt.mp4', `
+▶ ${statistic.playCount} Views
+❤ ${statistic.diggCount} Likes
+🔁 ${statistic.shareCount} Shares
+💬 ${statistic.commentCount} Comments
+- *By:* ${author.nickname} (${author.username})
+- *Desc:*
+${description}
+`.trim(), m)
 }
-handler.help = ['tiktok'].map(v => v + ' <url>')
+
+handler.help = ['tiktok <url>']
 handler.tags = ['downloader']
-handler.command = /^(tiktok)$/i
+handler.command = /^tiktok$/i
 
 module.exports = handler
